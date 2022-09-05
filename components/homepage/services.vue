7baa6bbee3cf24e626425273b7c42b3ff30b71d5
Ize Phanthakarn <izephanthakarn@hotmail.com>
2022-09-06 00:00:10 +0700

    service contact homepage

--- components/homepage/services.vue
+++ components/homepage/services.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full flex justify-center items-center overflow-x-hidden">
+  <div class="w-full flex flex-col justify-center items-center overflow-x-hidden">
     <div class="container px-5 flex flex-col justify-center items-center">
       <div class="title">
         <h1 class="head mx-auto">Services</h1>
@@ -7,21 +7,21 @@
       <div
         class="service-content mt-3 sm:mt-6 lg:px-20 w-full gap-4 2xl:flex 2xl:justify-center 2xl:items-center grid grid-cols-2 md:grid-cols-3 place-items-center text-default">
         <div class="w-full box aspect-square flex justify-center items-center">
-          <div class="content flex flex-col justify-center items-center">
+          <div class="content duration-300 flex flex-col justify-center items-center">
             <i class="uil-clapper-board text-5xl sm:text-8xl"></i>
             <p class="text-lg font-semibold">Front-end</p>
             <p class="text-base -mt-2">Nuxt.js</p>
           </div>
         </div>
         <div class="w-full box aspect-square flex justify-center items-center">
-          <div class="content flex flex-col justify-center items-center">
+          <div class="content duration-300 flex flex-col justify-center items-center">
             <i class="uil-processor text-5xl sm:text-8xl"></i>
             <p class="text-lg font-semibold">Back-end</p>
             <p class="text-base -mt-2">Golang</p>
           </div>
         </div>
         <div class="box rectang flex justify-center items-center col-span-2 md:col-span-1">
-          <div class="content flex md:flex-col justify-center items-center">
+          <div class="content duration-300 flex md:flex-col justify-center items-center">
             <i class="uil-cloud-share text-7xl sm:text-8xl"></i>
             <div class="flex flex-col items-center justify-center">
               <p class="text-lg font-semibold">Cloud server</p>
@@ -29,6 +29,17 @@
             </div>
           </div>
         </div>
+      </div>
+    </div>
+    <div class="contact container mt-8 flex flex-col sm:flex-row sm:rounded-3xl">
+      <div class="w-full sm:w-8/12 text-content px-6 pt-6 font-medium">
+        <h1 class="text-lg">You want to have a website ?</h1>
+        <h2 class="text-tiny mb-3">If this is your first project contact me and get 30% discount on your new project.
+        </h2>
+        <nuxt-link to="/contacts" class="button text-white rounded-xl py-1 px-3 text-tiny">Contact me</nuxt-link>
+      </div>
+      <div class="img-content w-full sm:w-4/12">
+        <img src="~/static/img/me/myprofile3.png" alt="" class="w-2/3 sm:w-full sm:rounded-3xl mr-0 ml-auto -mt-12 sm:mt-0">
       </div>
     </div>
   </div>
@@ -54,6 +65,10 @@ export default {
   box-shadow: 2px 2px 6px var(--color-shadow-btn);
 }
 
+.box:hover .content {
+  transform: scale(1.15);
+}
+
 i::before {
   transform: translateY(5px);
 }
@@ -66,6 +81,16 @@ i::before {
   aspect-ratio: 2/1;
 }
 
+.contact {
+  background: var(--bg-secondary);
+  box-shadow: 2px 2px 6px var(--color-shadow-btn);
+}
+
+.button {
+  background: var(--first-color);
+  box-shadow: 2px 2px 6px var(--color-shadow-btn);
+}
+
 @media(min-width:768px) {
   .box {
     max-width: 300px;
@@ -73,6 +98,23 @@ i::before {
 
   .rectang {
     aspect-ratio: 1/1;
+  }
+}
+
+@media (min-width: 1024px) {
+  .contact{
+    max-width: 824px;
+  }
+}
+@media(min-width:1280px){
+.contact{
+  max-width: 1030px;
+}
+}
+
+@media(min-width:1536px){
+  .contact{
+    max-width: 940px;
   }
 }
 </style>
